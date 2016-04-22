ea70469740e010528699664b1fb3e8bfc780021b
Ayesh Almeida <ayesh9206@gmail.com>
2016-04-22 14:05:03 +0530

    Interact - getting keybord into action - Ex2

--- examples/chap4/ex2/app.js
+++ examples/chap4/ex2/app.js
@@ -7,11 +7,18 @@ var main = function(){
 		if($(".comment-input input").val() !== ""){
 			$new_comment = $("<p>").text($(".comment-input input").val());
 			$(".comments").append($new_comment);
+			$(".comment-input input").val("");
 		}
 		else{
 			window.alert("No comments given. Please, add a comment.");
 		}
 	});
+
+	$(".comment-input input").on("keypress",function(event){
+		if(event.keyCode === 13){
+			console.log("Hello, I am the Key-bord" + event.keyCode);
+		}
+	})
 };
 
 $(document).ready(main);
