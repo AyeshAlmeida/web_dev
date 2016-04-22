527a8d52770a7dd01901fa9bd2d45146317095b4
Ayesh Almeida <ayesh9206@gmail.com>
2016-04-22 13:48:20 +0530

    Interact - part 4 - Ex2

--- examples/chap4/ex2/app.js
+++ examples/chap4/ex2/app.js
@@ -2,8 +2,12 @@ var main = function(){
 	"use strict";
 
 	$(".comment-input button").on("click",function(event){
-		var $new_comment = $("<p>");
-		$new_comment.text("This is hitero-genius comment.");
+		var $new_comment = $("<p>"),
+
+		comment_text = $(".comment-input input").val();
+
+		$new_comment.text(comment_text);
+		
 		$(".comments").append($new_comment);
 	});
 };
