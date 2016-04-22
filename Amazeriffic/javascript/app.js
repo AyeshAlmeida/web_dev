c5ed1e5177e677d4f6b7488508f1cdc46933d130
Ayesh Almeida <ayesh9206@gmail.com>
2016-04-22 20:07:10 +0530

    Making active tabs in Amazeriffic

--- Amazeriffic/javascript/app.js
+++ Amazeriffic/javascript/app.js
@@ -1,6 +1,33 @@
 var main = function(){
+
 	"use strict";
-	console.log("Hello World!");
+
+	$(".tabs a:nth-child(1)").on("click",function(){
+		//make all the tabs inactive
+		$(".tabs span").removeClass("active");
+		//make first tab active
+		$(".tabs a:nth-child(1) span").addClass("active");
+		//empty the main content - we can create it
+		$("main .content").empty();
+		//return false - we dont follow the link
+		return false;
+	});
+
+	$(".tabs a:nth-child(2)").on("click",function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
+
+	$(".tabs a:nth-child(3)").on("click",function(){
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
+		$("main .content").empty();
+		return false;
+	});
+
+
 };
 
 $(document).ready(main);
