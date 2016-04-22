f4cc9522c225f0db18dbcbbf04e677dec6e60e0b
Ayesh Almeida <ayesh9206@gmail.com>
2016-04-22 22:30:44 +0530

    Making active tabs(part 3) in Amazeriffic

--- Amazeriffic/javascript/app.js
+++ Amazeriffic/javascript/app.js
@@ -1,29 +1,15 @@
 var main = function(){
-
 	"use strict";
 
-	var activeTab = function(number){
-		var selectedtab = ".tabs a:nth-child("+number+") span";
-		$(".tabs span").removeClass("active");
-		$(selectedtab).addClass("active");
-	};
-
-	$(".tabs a:nth-child(1)").on("click",function(){
-		activeTab(1);
-		return false;
+	//listenr is impemented using for-each loop
+	$(".tabs a span").toArray().forEach(function (elements){
+		$(elements).on("click",function(){
+			$(".tabs a span").removeClass("active");
+			$(elements).addClass("active");
+			$(".main .content").empty();
+			return false;
+		});
 	});
-
-	$(".tabs a:nth-child(2)").on("click",function(){
-		activeTab(2);
-		return false;
-	});
-
-	$(".tabs a:nth-child(3)").on("click",function(){
-		activeTab(3);
-		return false;
-	});
-
-
 };
 
 $(document).ready(main);
