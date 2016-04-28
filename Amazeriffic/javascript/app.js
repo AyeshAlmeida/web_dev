1f30c6eae8269f4d7aec7e85f3f771b0d28b8147
Ayesh Almeida <ayesh9206@gmail.com>
2016-04-28 21:02:11 +0530

    New updates - 28-04-2016

--- Amazeriffic/javascript/app.js
+++ Amazeriffic/javascript/app.js
@@ -1,12 +1,35 @@
 var main = function(){
 	"use strict";
 
+	//todo-list
+	var todo = [
+		"Finish reading books.",
+		"Take Mom to park.",
+		"Answer emails.",
+		"Prep for Monday class.",
+		"Make some new ToDos.",
+		"Get groceries."
+	];
+
 	//listenr is impemented using for-each loop
-	$(".tabs a span").toArray().forEach(function (elements){
-		$(elements).on("click",function(){
+	$(".tabs a span").toArray().forEach(function (element){
+		$(element).on("click",function(){
+			
+			var $element = $(element);
+
 			$(".tabs a span").removeClass("active");
-			$(elements).addClass("active");
+			$(element).addClass("active");
 			$(".main .content").empty();
+
+			
+
+			if($element.parent().is(":nth-child(1)")){
+				console.log("First Element Selected.");
+			}else if($element.parent().is(":nth-child(2)")){
+				console.log("Second Element Selected.");
+			}else if($element.parent().is(":nth-child(3)")){
+				console.log("Third Element Selected.");
+			}
 			return false;
 		});
 	});
