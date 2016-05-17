914f500405b89453afa4b500a2e2ab1b6a99b535
ayesh123 <ayesh9206@gmail.com>
2016-05-17 23:33:09 +0530

    new 1

--- Amazeriffic/javascript/app.js
+++ Amazeriffic/javascript/app.js
@@ -1,52 +1,15 @@
 var main = function(){
 	"use strict";
-
-	//todo-list
-	var toDos = [
-		"Finish reading books.",
-		"Take Mom to park.",
-		"Answer emails.",
-		"Prep for Monday class.",
-		"Make some new ToDos.",
-		"Get groceries."
-	];
-
-	var tab;
-
-	//listener is impemented using for-each loop
-	var arr = $(".tabs a span").toArray();
-	for(tab=1;tab<=3;tab++){
-		var element = arr[tab];
 	
+	$(".tabs a span").toArray().forEach(function (element){
+		//creating a click-handler to elements
 		$(element).on("click",function (){
-			
-			var $element = $(element),$content;
-
 			$(".tabs a span").removeClass("active");
-			$(element).addClass("active");
-			$(".main .content").empty();
-
-			
-
-			if($element.parent().is(":nth-child(1)")){
-				console.log("First Element Selected.");
-			}else if($element.parent().is(":nth-child(2)")){
-				console.log("Second Element Selected.");
-
-				//adding new 'unordered-list'
-				$content = $("ul");
-				toDos.forEach(function (todo){
-					$content.append($("li").text(todo));
-				});
-
-				$("main .content").append($content);
-
-			}else if($element.parent().is(":nth-child(3)")){
-				console.log("Third Element Selected.");
-			}
+			$(element).addClasss("active");
+			$("main .content").empty();
 			return false;
 		});
-	};
+	});	
 };
 
 $(document).ready(main);
